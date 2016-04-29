6730ebb8ed729286cc3c659a412f25927f507cd4
Valentin Ryan Baca <valbaca@gmail.com>
2016-04-28 22:34:04 -0700

    basic CoreData read & write

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context = appDelegate.managedObjectContext
+        
+        // "Users" matches the Entity name
+        let newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        newUser.setValue("FooBar", forKey: "username") // these match the attributes
+        newUser.setValue("1234", forKey: "password")
+        
+        do {
+            try context.save()
+        } catch _ {
+            print("error saving")
+        }
+        
+        let request = NSFetchRequest(entityName: "Users")
+        
+        do {
+            let results = try context.executeFetchRequest(request)
+            print(results)
+        } catch _ {
+            print("error executing request")
+        }
     }
 
     override func didReceiveMemoryWarning() {
